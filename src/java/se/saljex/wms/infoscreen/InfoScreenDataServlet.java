0577dd63972fe6ffd7f1490cf31fc65032baed26
ulf <ulf@LAPTOP-RNVUDMQV.alltele.net>
2020-08-23 21:13:03 +0200

    infoscreen

---
+++ src/java/se/saljex/wms/infoscreen/InfoScreenDataServlet.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.saljex.wms.infoscreen;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.json.Json;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import se.saljex.wms.Const;
+
+/**
+ *
+ * @author ulf
+ */
+@WebServlet(name = "InfoScreenDataServlet", urlPatterns = {"/infoscreen/data"})
+public class InfoScreenDataServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Connection con=Const.getConnection(request);
+            PreparedStatement ps;
+            ResultSet rs;
+            
+            response.setContentType("text/event-stream");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter writer = response.getWriter();
+            
+            try {
+                ps = con.prepareStatement(getSql());
+                for (int i = 0; i < 100; i++) {
+                    rs = ps.executeQuery();
+                    
+                    String json = Json.createObjectBuilder()
+                    .add("ordrar", rs.getInt("ordrar"))
+                    .add("ordrarhotpick", rs.getInt("ordrarhotpick"))
+                    .add("ordrarhotpickdagens", rs.getInt("ordrarhotpickdagens"))
+                    .add("orderrader", rs.getInt("orderrader"))
+                    .add("hotpickorderrader", rs.getInt("hotpickorderrader"))
+                    .add("hotpickorderraderdagens", rs.getInt("hotpickorderraderdagens"))
+                    .add("plockadedagens", rs.getInt("plockadedagens"))
+                    .add("inlagradedagens", rs.getInt("inlagradedagens"))
+                    .add("plockade10dagar", rs.getInt("plockade10dagar"))
+                    .add("inlagrade10dagar", rs.getInt("inlagrade10dagar"))
+                    .add("plockade100dagar", rs.getInt("plockade100dagar"))
+                    .add("inlagrade100dagar", rs.getInt("inlagrade100dagar"))
+                    .build()
+                    .toString();
+
+                    writer.write("event:data\n");
+                    writer.write("data: "+ json + "\n\n");
+                    writer.flush();
+
+                    try {
+                            Thread.sleep(1000*60);
+                    } catch (InterruptedException e) { e.printStackTrace();       }
+                }
+            }
+            catch (Exception e) { e.printStackTrace(); }
+            finally { 
+                try { con.close(); } catch (Exception e) {} 
+                try { writer.close(); } catch (Exception e) {} 
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+
+    private String getSql() {
+        return "select *\n" +
+"from (\n" +
+"select \n" +
+"count(DISTINCT wo.Masterorderid) as ordrar,\n" +
+"count(DISTINCT case  when wo.Priority=4 then wo.masterorderid else null end) as ordrarhotpick,\n" +
+"count(DISTINCT case  when wo.Priority=4 and convert(date,wo.Creationdate)=convert(date,CURRENT_TIMESTAMP) then wo.masterorderid else null end) as ordrarhotpickdagens,\n" +
+"count(*) as orderrader,\n" +
+"sum (case when wo.Priority=4 then 1 else 0 end) as hotpickorderrader,\n" +
+"sum (case when wo.Priority=4 and convert(date,wo.Creationdate)=convert(date,CURRENT_TIMESTAMP) then 1 else 0 end) as hotpickorderraderdagens\n" +
+"\n" +
+"from Masterorder wo join Masterorderline wol on wol.MasterorderId = wo.MasterorderId\n" +
+"where wo.DirectionType=2 ) mo\n" +
+"join(\n" +
+"select \n" +
+"sum(case when type in (2,4) and convert(date, creationdate) = convert(date,CURRENT_TIMESTAMP)  then 1 else 0 end) as plockadedagens,\n" +
+"sum(case when type in (1,3) and convert(date, creationdate) = convert(date,CURRENT_TIMESTAMP) then 1 else 0 end) as inlagradedagens,\n" +
+"sum(case when type in (2,4) and creationdate >= dateadd(day,-10, CURRENT_TIMESTAMP) then 1 else 0 end) as plockade10dagar,\n" +
+"sum(case when type in (1,3) and creationdate >= dateadd(day,-10, CURRENT_TIMESTAMP) then 1 else 0 end) as inlagrade10dagar,\n" +
+"sum(case when type in (2,4) and creationdate >= dateadd(day,-100, CURRENT_TIMESTAMP) then 1 else 0 end) as plockade100dagar,\n" +
+"sum(case when type in (1,3) and creationdate >= dateadd(day,-100, CURRENT_TIMESTAMP) then 1 else 0 end) as inlagrade100dagar\n" +
+"from history \n" +
+"where QuantityConfirmed <> 0 and creationdate >= dateadd(day,-100, CURRENT_TIMESTAMP)  and type in (1,2,3,4)\n" +
+") h1 on 1=1";
+    }
+}
---
+++ web/infoscreen/index.jsp
@@ -0,0 +1,71 @@
+<%-- 
+    Document   : index
+    Created on : 2020-aug-23, 21:00:50
+    Author     : ulf
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>PPG Info</title>
+        <script>
+var source = new EventSource("data");
+source.addEventListener("data", 
+function(event) {
+    var o = JSON.parse(event.data);
+  document.getElementById("ordrar").innerHTML = o.ordrar;
+  document.getElementById("ordrarhotpick").innerHTML = o.ordrarhotpick;
+  document.getElementById("ordrarhotpickdagens").innerHTML = o.ordrarhotpickdagens;
+  document.getElementById("orderrader").innerHTML = o.orderrader;
+  document.getElementById("hotpickorderrader").innerHTML = o.hotpickorderrader;
+  document.getElementById("hotpickorderraderdagens").innerHTML = o.hotpickorderraderdagens;
+  document.getElementById("plockadedagens").innerHTML = o.plockadedagens;
+  document.getElementById("inlagradedagens").innerHTML = o.inlagradedagens;
+  document.getElementById("plockade10dagar").innerHTML = o.plockade10dagar;
+  document.getElementById("inlagrade10dagar").innerHTML = o.inlagrade10dagar;
+  document.getElementById("plockade100dagar").innerHTML = o.plockade100dagar;
+  document.getElementById("inlagrade100dagar").innerHTML = o.inlagrade100dagar;
+  if (o.hotpickorderrader>0 && flashVar==null) startFlash(); else if (o.hotpickorderrader===0 && flashVar != null) stopFlash();
+}, false);            
+
+    var flashVar = null;
+    var flashColor=1;
+function startFlash() {
+    flashVar = setInterval(flash, 1000);
+}
+function stopFlash() {
+    clearInterval(flashVar);
+    flashVar=null;
+    document.body.style.background = "white";
+}
+
+function flash() {
+    if (flashColor===1) {
+        color = "red";
+        flashColor=2;
+    } else {
+        color = "white";
+        flashColor=1;        
+    }
+    document.body.style.background = color;
+}
+            </script>
+        
+    </head>
+    <body>
+        Ordrar:<span id="ordrar"></span>
+        <br>ordrarhotpick:<span id="ordrarhotpick"></span>
+        <br>ordrarhotpickdagens:<span id="ordrarhotpickdagens"></span>
+        <br>orderrader:<span id="orderrader"></span>
+        <br>hotpickorderrader:<span id="hotpickorderrader"></span>
+        <br>hotpickorderraderdagens:<span id="hotpickorderraderdagens"></span>
+        <br>plockadedagens:<span id="plockadedagens"></span>
+        <br>inlagradedagens:<span id="inlagradedagens"></span>
+        <br>plockade10dagar:<span id="plockade10dagar"></span>
+        <br>inlagrade10dagar:<span id="inlagrade10dagar"></span>
+        <br>plockade100dagar:<span id="plockade100dagar"></span>
+        <br>inlagrade100dagar:<span id="inlagrade100dagar"></span>
+                       </body>
+</html>
